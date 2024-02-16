d6cda82ef65219e2cb3d3ed682c3e72d211baf71
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-16 11:57:15 +0100

    Update testdata.sql

--- database/testdata.sql
+++ database/testdata.sql
@@ -3,42 +3,35 @@ INSERT INTO ffvgs.Programms
 VALUES (
         1,
         "Fischen 1",
-        "Das ist fischen 1",
-        CURRENT_TIMESTAMP
+        "Das ist fischen 1"
     ),
     (
         2,
         "Fischen 2",
-        "Das ist fischen 2",
-        CURRENT_TIMESTAMP
+        "Das ist fischen 2"
     ),
     (
         3,
         "Fischen 3",
-        "Das ist fischen 3",
-        CURRENT_TIMESTAMP
+        "Das ist fischen 3"
     ),
     (
         4,
         "Fischen 4",
-        "Das ist fischen 4",
-        CURRENT_TIMESTAMP
+        "Das ist fischen 4"
     ),
     (
         5,
         "Fischen 5",
-        "Das ist fischen 5",
-        CURRENT_TIMESTAMP
+        "Das ist fischen 5"
     ),
     (
         6,
         "Fischen 6",
-        "Das ist fischen 6",
-        CURRENT_TIMESTAMP
+        "Das ist fischen 6"
     ),
     (
         7,
         "Fischen 7",
-        "Das ist fischen 7",
-        CURRENT_TIMESTAMP
+        "Das ist fischen 7"
     )
