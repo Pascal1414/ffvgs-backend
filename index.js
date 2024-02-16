0ae061c0a5800cd904c021f60675d2de7cf23496
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-16 12:05:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,36 @@ db.connect((err) => {
     console.log('Connected to database');
 });
 
-// Routes
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("This is the backend for ffvgs.ch");
+});
+
+// Programms
+app.get("/programms", (req, res) => {
+    db.query('SELECT * FROM programms', (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+app.get("/programms/:id", (req, res) => {
+    db.query('SELECT * FROM programms WHERE id = ?', [req.params.id], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.send(result[0]);
+        } else {
+            res.status(404);
+            res.send('Not found');
+        }
+    });
+});
+
+// Users
+app.get("/users", (req, res) => {
+    db.query('SELECT id, username, lastlogin FROM users', (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
 });
 
 
